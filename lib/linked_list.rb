2a716b78cd2ed6df47157f27459cc8fa484474ab
Charlie Fordon <fordoncf@gmail.com>
2022-02-23 18:14:30 -0500

    complete

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -37,7 +37,7 @@ class LinkedList
       array.push(current_node.value)
       current_node = current_node.next_node
     end
-    array
+    array.join(' -> ')
   end
 
   def size
@@ -70,6 +70,44 @@ class LinkedList
       it += 1
     end
     current_node.value
+  end
+
+  # I think that #pop could be better. I think that there is a reason that @tails is used, and using it would make #pop
+  # work better If #pop could reference the tail value, the tail could be set to nil and the the previous value could be
+  # set to tail
+
+  def pop
+    current_node = @head
+    while !current_node.next_node.next_node.nil?
+      current_node = current_node.next_node
+    end
+    saved_node = current_node.next_node
+    current_node.next_node = nil
+    saved_node.value
+  end
+
+  def contains?(value)
+    current_node = @head
+    answer = false
+    while !current_node.nil?
+      answer = true if current_node.value == value
+      current_node = current_node.next_node
+    end
+    answer
+  end
+
+  def find(value)
+    it = 0
+    current_node = @head
+    if contains?(value)
+      until current_node.value == value
+        current_node = current_node.next_node
+        it += 1
+      end
+      return it
+    else
+      return nil
+    end
   end
 
   class Node
--- lib/main.rb
+++ lib/main.rb
@@ -11,5 +11,13 @@ p my_list.size
 puts "The head value is: #{my_list.head}"
 puts "The tail value is: #{my_list.tail}"
 puts "The value at index 2 is: #{my_list.at(2)}"
+puts "removing value - '#{my_list.pop}' - from end of list..."
+p my_list.put_full_list
+puts "Does the list contain an value of 'first'? Answer: #{my_list.contains?('first')}"
+puts "Does the list contain an value of 'second'? Answer: #{my_list.contains?('second')}"
+puts "Does the list contain an value of 'fifth'? Answer: #{my_list.contains?('fifth')}"
+puts "The value 'first' is located at index: #{my_list.find('first')}"
+puts "The value 'Start' is located at index: #{my_list.find('Start')}"
+puts "The value 'fifth' is located at index: #{my_list.find('fifth')}"
 
 
